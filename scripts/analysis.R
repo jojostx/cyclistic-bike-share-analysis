9aabc9e8ef4d50ed5a334df8b0df1c6032c9a1e8
jojostx <ikuskid7@gmail.com>
2025-11-13 12:37:25 +0100

    Remove large CSV files and ignore them

---
+++ scripts/analysis.R
@@ -0,0 +1,17 @@
+# Purpose: Analyze tha cleaned Cyclistic 2024 CSV dataset
+
+# Load libraries
+
+# Load dataset CSV
+
+# split the data into buckets of day, start_hour, duration
+# split the data into buckets of start locations ranked by location with most rides starting there
+# split data according to average ride duration by hour, day, month, sort by count
+# Find top twenty locations (classify locations based on [residential, commercial/office, mixed]) for starting rides
+# identify top twenty locations (classify locations based on [residential, commercial/office, mixed]) for starting rides for different member types
+# split above relationships based on member type, rideable type
+# categories rides based on duration and member type and hour/day/month
+
+
+
+
